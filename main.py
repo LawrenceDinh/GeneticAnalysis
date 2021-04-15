aa56f43e1d7c3568ad465cb480f5a17a2dd3b686
Blank <blanks.media@gmail.com>
2021-04-15 12:18:10 -0700

    Finished scrape

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Bio import SeqIO
 from Bio import Entrez
 from Bio.Blast import NCBIWWW
 from Bio import SearchIO
+from lxml import etree
+from Bio.Blast import NCBIXML
 
 Entrez.email = "luat.dinh@sjsu.edu"
 
@@ -12,7 +14,6 @@ def readsequences(accession):
             for result in SeqIO.parse(request, "gb"):
                 nuc_des = str(result.description)
                 nuc_seq = str(result.seq)
-                print(len(nuc_seq))
     except:
         print("Request failed")
 
@@ -29,13 +30,15 @@ def writeBlast(blast_results, filename):
     blast_results.close()
 
 
-def processXML(filename):
-    blastresults = SearchIO.read(filename, 'blast-xml')
-    accessionArray = []
+def processXML(blastresults):
+    #(number, acc, key)
+    accessionArray ={}
+    i = 1
     for hit in blastresults:
         accession = getAccession(hit).id
-        print(hit.id)
-        accessionArray.append(accession)
+        # print(hit.id)
+        accessionArray[i] = [accession , hit, accession]
+        i = i+1
     return accessionArray
 
 def getAccession(hit):
@@ -50,8 +53,60 @@ def hitinfo(hit):
     scientificname = genbank.annotations['organism']
     return scientificname
 
-def refinedataset(hitarray):
-    print()
+
+def readXML(filename):
+    blastresults = SearchIO.read(filename, 'blast-xml')
+    return blastresults
+
+def createalignmentarray():
+    with open('basicblast.xml') as result_handle:
+        blast_record = NCBIXML.read(result_handle)
+    E_VALUE_THRESH = 0.04
+    alignmentarray = []
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+                alignmentarray.append(hsp.sbjct)
+    return alignmentarray
+
+def savespecies(resultsdict):
+    with open ("speciesarray.txt", "w") as f:
+        for key,val in resultsdict.items():
+            f.write(val[4] + "\n")
+    f.close()
+
+def addtodict(dictionary, array):
+    i = 0
+    for key, value in arr.items():
+        #speciesname = hitinfo(value[0])
+        #print(speciesname)
+        value.append(array[i])
+        #value.append(speciesname)
+        print(value)
+        i = i+1
+
+def readspecies(filename):
+    with open(filename, "r") as f:
+        writer = open("sparray.txt","w")
+        i = 1
+        for line in f:
+            sp = line.strip() + "," + str(i) + "\n"
+            #print(sp)
+            i = i+1
+            writer.write(sp)
+
+def finddiffspecies(filename):
+    sp = []
+    diffsp = []
+    results = []
+    with open(filename,"r") as f:
+        for line in f:
+            sp.append(line.strip().split(","))
+    for species in sp:
+        print(species[0])
+        if (species[0] not in diffsp):
+            diffsp.append(species[0])
+            results.append(species)
+    return results
 
 """
 An HSP has a query sequence (qseq), hit sequence (hseq) and midline--- '|' 
@@ -65,11 +120,17 @@ if __name__ == "__main__":
     """
     # writeBlast(runBlast("NM_020061.6", 100), "basicblast.xml")
     xmlFile = "basicblast.xml"
-    result_handle = open(xmlFile)
-
-    arr = processXML(xmlFile)
+    blastresults = readXML(xmlFile)
+    #getAlignment(blastresults)
+    arr = processXML(blastresults)
+    agn = createalignmentarray()
+    addtodict(arr, agn)
     print(arr)
-    for e in arr:
-        w = hitinfo(e)
-        print(w)
+    for key, val in arr.items():
+        print(val[1])
+    #savespecies(arr)
+    readspecies("speciesarray.txt")
+    uniquespecies = finddiffspecies("sparray.txt")
+    for x in uniquespecies:
+        print(x)
 
