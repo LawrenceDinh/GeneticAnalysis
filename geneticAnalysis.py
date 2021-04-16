2379815a2e0ff3be026b2469f80a68e0b214ee87
Blank <blanks.media@gmail.com>
2021-04-15 21:48:39 -0700

    Convert Clustal MSA to Fasta

--- geneticAnalysis.py
+++ geneticAnalysis.py
@@ -7,19 +7,32 @@ from Bio.Align.Applications import ClustalwCommandline
 
 basic_set = "basicset.fasta"
 related_set = "OPN1LW_refseq_transcript.fasta"
-basicset_alignment_output = "basicaligned.fasta"
-relatedset_alignment_output = "relatedaligned.fasta"
+basicset_alignment_output = "basicaligned.clustal"
+relatedset_alignment_output = "relatedaligned.aln"
+
+def read_fastafile(fastafile):
+    return list(SeqIO.parse(fastafile, "fasta"))
 
 def clustalW_alignment(fastafile, alignment_output):
-    basic_data_set = list(SeqIO.parse(fastafile, "fasta"))
-    related_data_set = list(
-        SeqIO.parse("OPN1LW_refseq_transcript.fasta", "fasta"))
+    basic_data_set = read_fastafile(fastafile)
+    related_data_set = read_fastafile("OPN1LW_refseq_transcript.fasta")
 
     cline = ClustalwCommandline(r"C:\Program Files (x86)\ClustalW2\clustalw2",
                                 infile=fastafile, outfile=alignment_output)
     cline()
     print(cline)
 
+def convertClustal_toFASTA(clustalalignment):
+    records = SeqIO.parse(clustalalignment, "clustal")
+    count = SeqIO.write(records, "convertedbasicalignment.fasta", "fasta")
+
 if __name__ == "__main__":
     clustalW_alignment(basic_set, basicset_alignment_output)
-    clustalW_alignment(related_set, relatedset_alignment_output)
+    #clustalW_alignment(related_set, relatedset_alignment_output)
+    with open(basicset_alignment_output, "r") as align:
+        alignment = AlignIO.read(align, "clustal")
+    i = 0
+    for rec in alignment:
+        print(rec, i )
+        i = i + 1
+    convertClustal_toFASTA("basicaligned.aln")
